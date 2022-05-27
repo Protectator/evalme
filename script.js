3d33bb50c3d8660645d67f80d1a26b9915b3a495
Kewin Dousse <kewin.dousse@qoqa.com>
2022-05-27 21:29:34 +0200

    POC

---
+++ script.js
@@ -0,0 +1,21 @@
+const regexp = /\{\{(.*)\}\}/;
+
+function run() {
+    const url = window.location;
+    const search = url.search;
+    if (search === "") {
+        window.location = 'help.html';
+        return;
+    }
+    const params = new URLSearchParams(window.location.search);
+    const s = params.get('s');
+    if (!s) {
+        window.location = 'help.html';
+        return;
+    }
+    const urlTemplate = decodeURI(s);
+    const computedURL = urlTemplate.replace(regexp, (e) => {return eval(e)});
+    console.log(computedURL);
+    window.location = encodeURI(computedURL);
+}
+run();
